c42831c32640aacc334cc53fd592c78d22bbc7ec
Utkarsh Deep <utkarsh_d@LT-31155.local>
2020-09-16 16:50:01 +0530

    asyncMiddleware fix

--- templates/utils/async_middleware.js
+++ templates/utils/async_middleware.js
@@ -1,1 +1,1 @@
-export const asyncMiddleware = (fn) => (req, res, next) => Promise.resolve(fn(req, res, next)).then(_ => next()).catch(next)
+export const asyncMiddleware = (fn) => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
